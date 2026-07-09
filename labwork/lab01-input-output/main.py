f7fa81caa06fb9405dce46e1b2999e3bc66e997a
jfchrist <jfchrist28@gmail.com>
2026-07-09 05:33:34 +0000

    finished!

--- labwork/lab01-input-output/main.py
+++ labwork/lab01-input-output/main.py
@@ -1,6 +1,6 @@
 # DEV 108 Lab Activity 1
-# [Today's Date]
-# [Your Name]
+# 7/6/2026
+# Jaine Christ
 
 # Start writing your code below.
 # Click the "Run" button (or run this file in the terminal with: python main.py)
@@ -8,3 +8,25 @@
 # Good luck! You can do this - please don't use any AI tools to do this assignment. 
 # That violates the AI Policy for this class and puts your learning at a disadvantage. 
 
+print("Hello world!")
+print("28 + 32 =",28+32)
+print(f"28+32={28+32}")
+num1=28
+num2=32
+num3=3
+print(f"{num1} x {num2} x {num3} = {num1*num2*num3}")
+firstName="Jaine"
+lastName="Christ"
+major="Business Management"
+print(f"My name is {firstName} {lastName}.")
+print("My name is",firstName,lastName,".")
+print(f"My major is {major}.")
+num1=int(input("Enter a number from 0 to 100: "))
+print("You entered",num1,".")
+num2=input("Enter a number from 10 to 10000: ")
+print("You entered",num2,".")
+num2=int(num2)
+print(f"{num1} x {num2} = {num1*num2}")
+happiness=int(input("On a scale of 1 to 10, how happy are you?:"))
+happy="HAPPY"
+print(f"You answered {happy*happiness}")
