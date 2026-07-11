0db8ca9b59f5730ad0b4103623e03def59821f53
jfchrist <jfchrist28@gmail.com>
2026-07-11 04:57:47 +0000

    finished lab 2-1

--- labwork/lab02-01-testscores/main.py
+++ labwork/lab02-01-testscores/main.py
@@ -11,17 +11,20 @@ print("Enter 3 test scores")
 print("======================")
 
 # get scores from the user
-total_score = 0       # initialize the variable for accumulating scores
-total_score += int(input("Enter test score: "))
-total_score += int(input("Enter test score: "))
-total_score += int(input("Enter test score: "))
+      # initialize the variable for accumulating scores
+score1 = int(input("Enter test score: "))
+score2 = int(input("Enter test score: "))
+score3 = int(input("Enter test score: "))
+
+total_score = score1+score2+score3
 
 # calculate average score
 average_score = round(total_score / 3)
              
 # format and display the result
 print("======================")
-print("Total Score:  ", total_score,
+print("Your Scores: ", score1 , score2 , score3,
+      "\nTotal Score: ", total_score,
       "\nAverage Score:", average_score)
 print()
 print("Bye")
