336688c931036e818a8351a1ffe52ef09e19ae75
jfchrist <jfchrist28@gmail.com>
2026-07-19 05:19:46 +0000

    lab 04 - 01

--- labwork/lab04-01-mpg/main.py
+++ labwork/lab04-01-mpg/main.py
@@ -1,23 +1,41 @@
 # Starting file for Exercise 3-1
-
 # display a welcome message
-print("The Miles Per Gallon application")
-print()
-
+print("The Miles Per Gallon Program")
 # get input from the user
-miles_driven = float(input("Enter miles driven:         "))
-gallons_used = float(input("Enter gallons of gas used:  "))
-
-if miles_driven <= 0:
-    print("Miles driven must be greater than zero. Please try again.")
-elif gallons_used <= 0:
-    print("Gallons used must be greater than zero. Please try again.")
-else:
+while True:     #establish loop for incorrect answers or 2nd run of program
+    print()
+    miles_driven = float(input("Please enter miles driven:         "))
+    gallons_used = float(input("Please enter gallons of gas used:  "))
+    gallon_cost = float(input("Please enter cost per gallon:     "))
+# validates user inputs
+    if miles_driven <= 0:
+        print("Miles driven must be greater than zero. Please try again.")
+        continue
+    elif gallons_used <= 0:
+        print("Gallons used must be greater than zero. Please try again.")
+        continue
+    elif gallon_cost <= 0:
+        print("Gallon cost must be greater than zero. Please try again.")
+        continue
+    else:
     # inputs must be good
     # calculate and display miles per gallon
-    mpg = round((miles_driven / gallons_used), 2)
-    print("Miles Per Gallon:          ", mpg)
-
+        print()
+        print("==="*5)
+        mpg = round((miles_driven / gallons_used), 2)
+        print("Miles Per Gallon:   ", mpg)
+    # calculate and display total gas cost
+        total_cost = round(gallons_used*gallon_cost)
+        print(f"Total Cost:         ${float(total_cost)}")
+    #calculate and display cost per mile
+        print(f"Cost per Mile:      ${round(total_cost/miles_driven,2)}")
+        print("==="*5)
+    #ask for loop
+        answer = input("\nWould you like to enter information for another trip? y/n:  ")
+        if answer.lower() == "y":
+            continue
+        else:
+            break
 print()
-print("Bye")
+print("Good-Bye!")
 
