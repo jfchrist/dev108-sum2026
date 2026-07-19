463c2dd6a52c0d385b3d10c07ed853417290a649
jfchrist <jfchrist28@gmail.com>
2026-07-19 03:33:08 +0000

    commiting changes

--- labwork/lab02-02-calculator/main.py
+++ labwork/lab02-02-calculator/main.py
@@ -1,2 +1,12 @@
 # this is the starting file for Lab Activity 2.2
 # create your own starting code for the instructions provided 
+
+print("The Area and Perimeter Program")
+print("==============================")
+length=int(input("What is the length of your rectangle?: "))
+width=int(input("What is the width of your rectangle?: "))
+print("==============================")
+print(f"Area = {length*width}")
+print(f"Perimeter = {(length + width)*2}")
+print("==============================")
+print("Thank you for using this program! ▯")
