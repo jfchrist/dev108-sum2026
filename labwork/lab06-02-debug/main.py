9365126de72c08611217266eb851c47a8cf9bcc0
jfchrist <jfchrist28@gmail.com>
2026-07-26 02:35:10 +0000

    finished lab 6-2

--- labwork/lab06-02-debug/main.py
+++ labwork/lab06-02-debug/main.py
@@ -13,15 +13,18 @@ test_score = 0
 while True:
     test_score = input("Enter test score (or 'x' to quit): ")
     if test_score != "x":
-        test_score = float(test_score)
-        score_total += test_score
-        counter += 1
+        if test_score.isdigit():
+            test_score = float(test_score)
+            if test_score >= 0 and test_score <= 100:
+                score_total += test_score
+                counter += 1
+                continue
+            else:
+                print("Test score must be valid integer from 0 through 100. Score discarded. Try again.")
+        else:
+            print("System cannot recognize input. Please try again with valid integer.")
     else:
         break
-    if test_score >= 0 and test_score <= 100:
-        continue
-    else:
-        print("Test score must be from 0 through 100. Score discarded. Try again.")   
 
 # calculate average score
 average_score = round(score_total / counter)
@@ -36,10 +39,13 @@ print("Bye")
 #Test Case Values
 #Test Number    Inputs      Expected Outputs    Actual Outputs  Pass/Fail
 # 1             100, 90, 80     270, 90             270, 45       Fail
-# ** Changed: moved erased second counter += 1 and moved adding total plus score to same first 'if' statement
+# ** Changed: moved erased second counter += 1 and moved adding total plus score to same first 'if' statement; also noticed
+#and moved filter for numbers less than 0 or over 100 so they get filtered out before adding them to the toal score
 # 2             100, 90, 80     270, 90             270, 90       Pass
 # 3             100, 98, 80     278, 93             278, 93       Pass
 # 4             100, 90.5, 95   285.5, 95.2         ERROR         Fail
 # ** Changed: system to recognize tes_score variable as float not int
-# 5             100, 90.5, 95   285.5, 95.2         285.5, 95
-#
+# 5             100, 90.5, 95   285.5, 95           285.5, 95     Pass
+# 6             cat             invalid message     ERROR         Fail
+# ** Changed: system checks if input is digits and displays error message without affecting variables
+# 7             cat, 111, 100   100, 100            100, 100      Pass
